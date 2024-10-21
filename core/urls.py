9a064099f660569e2100c444e153046c6e48341c
matheusfabiao <matheusfabiaopereira@gmail.com>
2024-10-20 22:21:51 -0300

    feat: user login form & view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('__reload__/', include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
-    path('', include('website.urls'))
+    path('', include('website.urls')),
 ]
